5125909b7a77011bda7958d77220fcbc4c34f5b9
Masaya Kataoka <ms.kataoka@gmail.com>
2018-10-13 09:37:01 +0900

    add launch and config files

--- src/graph_slam.cpp
+++ src/graph_slam.cpp
@@ -20,6 +20,17 @@ graph_slam::~graph_slam()
 void graph_slam::pointcloud_callback_(const sensor_msgs::PointCloud2::ConstPtr msg)
 {
     std::lock_guard<std::mutex> lock(sensor_mutex_);
+    geometry_msgs::TransformStamped transform_stamped;
+    sensor_msgs::PointCloud2 pointcloud_transformed;
+    try
+    {
+        transform_stamped = tf_buffer_.lookupTransform(lidar_frame_, robot_frmae_ , ros::Time(0));
+    }
+    catch(tf2::TransformException &ex)
+    {
+        ROS_WARN_STREAM("Could NOT transform " << lidar_frame_ << " to " << robot_frmae_ << " " << ex.what());
+        return;
+    }
     return;
 }
 
@@ -28,6 +39,7 @@ void graph_slam::imu_callback_(const sensor_msgs::Imu::ConstPtr msg)
     std::lock_guard<std::mutex> lock(sensor_mutex_);
     geometry_msgs::TransformStamped transform_stamped;
     geometry_msgs::Vector3Stamped linear_acc_stamped;
+    linear_acc_stamped.vector = msg->linear_acceleration;
     try
     {
         transform_stamped = tf_buffer_.lookupTransform(imu_frame_, robot_frmae_ , ros::Time(0));
