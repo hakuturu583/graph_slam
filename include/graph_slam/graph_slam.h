22f3fdc5daa41411dda9fdf9e0ed98d14346ec36
Masaya Kataoka <ms.kataoka@gmail.com>
2018-10-13 09:27:33 +0900

    add imu transform function

--- include/graph_slam/graph_slam.h
+++ include/graph_slam/graph_slam.h
@@ -4,7 +4,15 @@
 //headers in ROS
 #include <ros/ros.h>
 #include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/Vector3Stamped.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/Imu.h>
+#include <nmea_msgs/Sentence.h>
+
+//headers in STL
+#include <mutex>
 
 class graph_slam
 {
@@ -12,6 +20,7 @@ public:
     graph_slam();
     ~graph_slam();
 private:
+    std::mutex sensor_mutex_;
     ros::NodeHandle nh_;
     ros::Subscriber pointcloud_sub_;
     std::string pointcloud_topic_name_;
@@ -25,6 +34,10 @@ private:
     std::string imu_frame_;
     std::string gps_frame_;
     std::string lidar_frame_;
+    std::string map_frame_;
+    void imu_callback_(const sensor_msgs::Imu::ConstPtr msg);
+    void nmea_callback_(const nmea_msgs::Sentence::ConstPtr msg);
+    void pointcloud_callback_(const sensor_msgs::PointCloud2::ConstPtr msg);
 };
 
 #endif  //GRAPH_SLAM_H_INCLUDED
--- src/graph_slam.cpp
+++ src/graph_slam.cpp
@@ -6,9 +6,42 @@ graph_slam::graph_slam() : tf_listener_(tf_buffer_)
     nh_.param<std::string>(ros::this_node::getName()+"/imu_frame", imu_frame_, "imu");
     nh_.param<std::string>(ros::this_node::getName()+"/gps_frame", gps_frame_, "gps");
     nh_.param<std::string>(ros::this_node::getName()+"/lidar_frame", lidar_frame_, "velodyne");
+    nh_.param<std::string>(ros::this_node::getName()+"/map_frame", map_frame_, "map");
+    pointcloud_sub_ = nh_.subscribe(ros::this_node::getName()+"/pointcloud",10, &graph_slam::pointcloud_callback_, this);
+    imu_sub_ = nh_.subscribe(ros::this_node::getName()+"/imu",10, &graph_slam::imu_callback_, this);
+    nmea_sub_ = nh_.subscribe(ros::this_node::getName()+"/nmea_sentence",10, &graph_slam::nmea_callback_, this);
 }
 
 graph_slam::~graph_slam()
 {
     
+}
+
+void graph_slam::pointcloud_callback_(const sensor_msgs::PointCloud2::ConstPtr msg)
+{
+    std::lock_guard<std::mutex> lock(sensor_mutex_);
+    return;
+}
+
+void graph_slam::imu_callback_(const sensor_msgs::Imu::ConstPtr msg)
+{
+    std::lock_guard<std::mutex> lock(sensor_mutex_);
+    geometry_msgs::TransformStamped transform_stamped;
+    geometry_msgs::Vector3Stamped linear_acc_stamped;
+    try
+    {
+        transform_stamped = tf_buffer_.lookupTransform(imu_frame_, robot_frmae_ , ros::Time(0));
+    }
+    catch(tf2::TransformException &ex)
+    {
+        ROS_WARN_STREAM("Could NOT transform " << imu_frame_ << " to " << robot_frmae_ << " " << ex.what());
+        return;
+    }
+    tf2::doTransform(linear_acc_stamped, linear_acc_stamped, transform_stamped);
+    return;
+}
+
+void graph_slam::nmea_callback_(const nmea_msgs::Sentence::ConstPtr msg)
+{
+    return;
 }
